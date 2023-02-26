2767f814b1066842fa9c6050889c83aaccd6382b
markb500 <81427572+markb500@users.noreply.github.com>
2023-02-26 15:53:55 +0000

    Add files via upload

---
+++ areavol_module.js
@@ -0,0 +1,284 @@
+var prevsum = 0, prev2sum = 0, sumq, suma, a, b, c, d, e, f, g;
+var units, unitsmath, sacub, satri, satube, saend, salgcyl, sasmcyl, sahd, vcub, vtri, vtube, vlgcyl, vsmcyl, vhd;
+function areavol() {
+    //Uses 1 of 5 shapes with random dimensions added. Surface area & volume to be calculated.
+    var sum, left = 75, top = 25, right = left + 350, bottom = top + 200;
+    sumq = "";
+    suma = "";
+    suma+= "<br>".repeat(13);
+    switch(rndgen(1, 3, 0, 1, -1)) {      //1=m 2=cm 3=mm
+        case 1:
+            units = " m";
+            unitsmath = "\\ m";
+            break;
+        case 2:
+            units = " cm";
+            unitsmath = "\\ cm";
+            break;
+        case 3:
+            units = " mm";
+            unitsmath = "\\ mm";
+            break;
+    }
+    document.getElementById("myCanvas");
+    myCanvas.width = "500";
+    myCanvas.height = "300";
+    myCanvas.style = "border: none;"; //1px solid #000000 
+    document.getElementById("a").innerHTML = "";
+    // document.getElementById("noteslink").style.visibility="visible";
+    // document.getElementById("noteslink").onclick = function() {
+    //     window.open("images/20200505-MathsBook12AreaVolv1_3-APO.pdf#page=3", "_blank")
+    // }
+    var ctx = myCanvas.getContext("2d");
+    ctx.font = "20px Comic Sans MS";
+    sumq += "Find the surface area and volume of the object shown, " + 
+                "rounding your answers to 1 decimal place. Drawing is not to scale. <br><br>";
+    
+    do {
+        sum = rndgen(1, 5, 0, 1, -1);
+    } while(sum === prevsum || sum === prev2sum)
+    prev2sum = prevsum;
+    prevsum = sum;
+    switch(sum) {
+        case 1:     //cubtri
+            do {
+                a = rndgen(8, 21, 0, 1, -1);   //a int 8 to 21
+                e = rndgen(4, a - 3, 2, 0.25, -1); //e 4 to (a - 3) in 0.25's
+                d = rndgen(5, 15, 0, 1, -1);   //d int 5 to 15
+                c = rndgen(3, d - 2, 2, 0.25, -1); //c 3 to (d - 2) in 0.25's
+                b = rndgen(4, 12, 0, 1, -1);   //b int 4 to 12
+                f = (Math.floor(Math.sqrt((Math.pow((a - e), 2) + 
+                            Math.pow((d - c), 2))) * 2) * 5) / 10;//f sqrt((a - e)^2 + (d - c)^2) in 0.5's
+            } while(a === b || a === d || b === d || e === d || f === d)
+            sacub = dp(2 * a * c + 2 * b * c + a * b + e * b, 3, 2);
+            satri = dp((a - e) * (d - c) + b * f + b * (d - c), 3, 2);
+            vcub = dp(a * b * c, 3, 2);
+            vtri = dp(0.5 * (a - e) * (d - c) * b, 3, 2);
+            var img = document.getElementById("cubtri");
+            ctx.drawImage(img, left, top, 350, 200);
+            ctx.textAlign = "center";
+            ctx.fillText(a + units, left + 145, bottom + 15);   //a
+            ctx.textAlign = "right";
+            ctx.fillText(d + units, left - 2, (bottom - top) / 2 + 60);  //d
+            ctx.textAlign = "left";
+            ctx.fillText(b + units, right - 40, bottom - 25);   //b
+            ctx.textAlign = "left";
+            ctx.fillText(c + units, right - 10, bottom - 90);   //c
+            ctx.textAlign = "center";
+            ctx.fillText(e + units, left + 265, top + 65);   //e
+            ctx.textAlign = "left";
+            ctx.fillText(f + units, left + 135, top + 30);   //f
+            suma += "$$\\begin{aligned}SA\\ of\\ Cuboid&=2\\times side+2\\times end+base+top\\\\[5pt]";
+            suma += "&=2\\times" + a + "\\times" + c + "+2\\times" + b + "\\times" + c + "+" + a + 
+                        "\\times" + b + "+" + e + "\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(sacub, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ triangular\\ prism&=2\\times triangular\\ ends+slope+side\\\\[5pt]";
+            suma += "&=2\\times \\frac{1}{2}\\times" + (a - e) + "\\times" + (d - c) + "+" + 
+                            b + "\\times" + f + "+" + b + "\\times" + (d - c) + "\\\\[5pt]";
+            suma += "&=" + thouSep(satri, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ total&=" + thouSep(sacub, "\\ ") + "+" + thouSep(satri, "\\ ") + "=\\underline{\\mathbf{" + 
+                    thouSep(dp(Number(sacub) + Number(satri), 3, 1), "\\ ") + unitsmath + "^2 \\ (1\\ dp)}}\\\\[20pt]"; 
+            suma += "V\\ of\\ cuboid&=length\\times width\\times height\\\\[5pt]";
+            suma += "&=" + a + "\\times" + b + "\\times" + c + "\\\\[5pt]";
+            suma += "&=" + thouSep(vcub, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ of\\ triangular\\ prism&=area\\ of\\ triangle\\times length\\ of\\ prism\\\\[5pt]";
+            suma += "&=\\frac{1}{2}\\times" + (a - e) + "\\times" + (d - c) + "\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(vtri, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ total&=" + thouSep(vcub, "\\ ") + "+" + thouSep(vtri, "\\ ") + "=\\underline{\\mathbf{" + 
+                    thouSep(dp(Number(vcub) +  Number(vtri), 3, 1), "\\ ") + unitsmath + "^3 \\ (1\\ dp)}}\\end{aligned}$$"; 
+            break;
+        case 2:     //cubtritube
+            do {
+                a = rndgen(8, 21, 0, 1, -1);   //a int 8 to 21
+                e = rndgen(4, a - 3, 2, 0.25, -1); //e 4 to (a - 3) in 0.25's
+                d = rndgen(5, 15, 0, 1, -1);   //d int 5 to 15
+                c = rndgen(3, d - 2, 2, 0.25, -1); //c 3 to (d - 2) in 0.25's
+                b = rndgen(4, 12, 0, 1, -1);   //b int 4 to 12
+                f = (Math.floor(Math.sqrt((Math.pow((a - e), 2) + 
+                            Math.pow((d - c), 2))) * 2) * 5) / 10;//f sqrt((a - e)^2 + (d - c)^2) in 0.5's
+                g = rndgen(2, c - 1, 1, 0.5, -1);//g (2 to (c - 1) in 0.5's)
+            } while(a === b || a === d || b === d || e === d || f === d)
+            sacub = dp(2 * a * c + 2 * b * c + a * b + e * b, 3, 2);
+            satri = dp((a - e) * (d - c) + b * f + b * (d - c), 3, 2);
+            satube = dp(Math.PI * g * a, 3, 2);
+            saend = dp(2 * Math.PI * Math.pow(g / 2, 2), 3, 2);
+            vcub = dp(a * b * c, 3, 2);
+            vtri = dp(0.5 * (a - e) * (d - c) * b, 3, 2);
+            vtube = dp(Math.PI * Math.pow((g / 2), 2) * a, 3, 2);
+            var img = document.getElementById("cubtritube");
+            ctx.drawImage(img, left, top, 350, 200);
+            ctx.textAlign = "center";
+            ctx.fillText(a + units, left + 145, bottom + 15);   //a
+            ctx.textAlign = "right";
+            ctx.fillText(d + units, left - 2, (bottom - top) / 2 + 60);  //d
+            ctx.textAlign = "left";
+            ctx.fillText(b + units, right - 40, bottom - 25);   //b
+            ctx.textAlign = "left";
+            ctx.fillText(c + units, right - 10, bottom - 90);   //c
+            ctx.textAlign = "center";
+            ctx.fillText(e + units, left + 265, top + 65);   //e
+            ctx.textAlign = "left";
+            ctx.fillText(f + units, left + 135, top + 30);   //f
+            ctx.textAlign = "right";
+            ctx.fillText("Diameter " + g + units, right + 40, top + 30);   //g
+            suma += "$$\\begin{aligned}SA\\ of\\ Cuboid&=2\\times side+2\\times end+base+top\\\\[5pt]";
+            suma += "&=2\\times" + a + "\\times" + c + "+2\\times" + b + "\\times" + c + "+" + a + 
+                        "\\times" + b + "+" + e + "\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(sacub, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ triangular\\ prism&=2\\times triangular\\ ends+slope+side\\\\[5pt]";
+            suma += "&=2\\times \\frac{1}{2}\\times" + (a - e) + "\\times" + (d - c) + "+" + 
+                            b + "\\times" + f + "+" + b + "\\times" + (d - c) + "\\\\[5pt]";
+            suma += "&=" + thouSep(satri, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ tube&=\\pi \\times diameter\\times length\\\\[5pt]";
+            suma += "&=\\pi \\times" + g + "\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(satube, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ end\\ caps&=2\\times \\pi \\times radius^2\\\\[5pt]";
+            suma += "&=2\\times \\pi \\times" + (g / 2) + "^2\\\\[5pt]";
+            suma += "&=" + thouSep(saend, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ total&=" + thouSep(sacub, "\\ ") + "+" + satri + "+" + satube + "-" + saend + "=\\underline{\\mathbf{" + 
+                        thouSep(dp(Number(sacub) + Number(satri) + Number(satube) - Number(saend), 3, 1), "\\ ") + 
+                        unitsmath + "^2 \\ (1\\ dp)}}\\\\[20pt]"; 
+            suma += "V\\ of\\ cuboid&=length\\times width\\times height\\\\[5pt]";
+            suma += "&=" + a + "\\times" + b + "\\times" + c + "\\\\[5pt]";
+            suma += "&=" + thouSep(vcub, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ of\\ triangular\\ prism&=area\\ of\\ triangle\\times length\\ of\\ prism\\\\[5pt]";
+            suma += "&=\\frac{1}{2}\\times" + (a - e) + "\\times" + (d - c) + "\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(vtri, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ of\\ tube&=\\pi \\times radius^2\\times length\\\\[5pt]";
+            suma += "&=\\pi\\times" + (g / 2) + "^2\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(vtube, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ total&=" + thouSep(vcub, "\\ ") + "+" + thouSep(vtri, "\\ ") + "-" + thouSep(vtube, "\\ ") + 
+                        "=\\underline{\\mathbf{" + thouSep(dp(Number(vcub) + Number(vtri) - Number(vtube), 3, 1), "\\ ") + 
+                        unitsmath + "^3 \\ (1\\ dp)}}\\end{aligned}$$";
+            break;
+        case 3:     //2cyl
+            do {
+                a = rndgen(7, 25, 1, 0.5, -1);//7 to 25 in 0.5's  lg cyl length
+                b = rndgen(3, 15, 1, 0.5, -1);//3 to 15 in 0.5's  sm cyl length
+                c = rndgen(2, 7, 1, 0.5, -1);//2 to 7 in 0.5's  sm cyl dia
+                d = rndgen(c + 3, 12, 1, 0.5, -1);//(c+3) to 12 in 0.5's lg cyl dia
+            } while(a === b || a === d || b === c || b === d)
+            salgcyl = dp(2 * Math.PI * Math.pow((d / 2), 2) + Math.PI * d * a, 3, 2);
+            sasmcyl = dp(Math.PI * c * b, 3, 2);
+            vlgcyl = dp(Math.PI * Math.pow((d / 2), 2) * a, 3, 2);
+            vsmcyl = dp(Math.PI * Math.pow((c / 2), 2) * b, 3, 2);
+            var img = document.getElementById("2cyl");
+            ctx.drawImage(img, 75, 25, 350, 200);
+            ctx.textAlign = "center";
+            ctx.fillText(a + units, left + 120, bottom + 15);   //a
+            ctx.fillText(b + units, left + 260, bottom + 15);   //b
+            ctx.textAlign = "left";
+            ctx.fillText(c + units, right, top + 105);   //c
+            ctx.textAlign = "right";
+            ctx.fillText(d + units, left - 2, top + 105);  //d
+            suma += "$$\\begin{aligned}SA\\ of\\ large\\ cylinder&=2\\times end\\ caps+tube\\\\[5pt]";
+            suma += "&=2\\times \\pi \\times" + (d / 2) + "^2+\\pi \\times" + d + "\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(salgcyl, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ small\\ cyclinder&=small\\ tube\\ (area\\ of\\ 2\\ small\\ end\\ caps\\ " + 
+                        "hidden\\ in\\ joint)\\\\[5pt]";
+            suma += "&=\\pi \\times" + c + "\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(sasmcyl, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ total&=" + thouSep(salgcyl, "\\ ") + "+" + sasmcyl + "=\\underline{\\mathbf{" + 
+                    thouSep(dp(Number(salgcyl) + Number(sasmcyl), 3, 1), "\\ ") + unitsmath + "^2 \\ (1\\ dp)}}\\\\[20pt]";
+            suma += "V\\ of\\ large\\ cyclinder&=area\\ of\\ large\\ circle\\times length\\\\[5pt]";
+            suma += "&=\\pi \\times" + (d / 2) + "^2\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(vlgcyl, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ of\\ small\\ cyclinder&=area\\ of\\ small\\ circle\\times length\\\\[5pt]";
+            suma += "&=\\pi \\times" + (c / 2) + "^2\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(vsmcyl, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ total&=" + thouSep(vlgcyl, "\\ ") + "+" + thouSep(vsmcyl, "\\ ") + "=\\underline{\\mathbf{" + 
+                    thouSep(dp(Number(vlgcyl) + Number(vsmcyl), 3, 1), "\\ ") + unitsmath + "^3 \\ (1\\ dp)}}\\end{aligned}$$";
+            break;
+        case 4:     //tritube
+            do {
+                a = rndgen(8, 25, 0, 1, -1);   //8 to 25 int
+                b = rndgen(a - 2, 15, 0, 1, -1);   //(a - 2) to 15 int
+                c = rndgen(a - 2, 15, 0, 1, -1);   //(a - 2) to 15 int not = b
+                d = rndgen(2, b - 3, 0, 1, -1);   //2 to (b - 3) int
+                e = Math.floor(Math.sqrt(Math.pow(b / 2, 2) + Math.pow(c, 2)));   //pythag b/2 & c
+            } while(a === b || a === c || b === c || a === e)
+            satri = dp(b * c + 2 * a * e + a * b, 3, 2);
+            satube = dp(Math.PI * d * a, 3, 2);
+            saend = dp(2 * Math.PI * Math.pow((d / 2), 2), 3, 2);
+            vtri = dp(0.5 * b * c * a, 3, 2);
+            vtube = dp(Math.PI * Math.pow((d / 2), 2) * a, 3, 2);
+            var img = document.getElementById("tritube");
+            ctx.drawImage(img, 75, 25, 350, 200);
+            ctx.textAlign = "right";
+            ctx.fillText(a + units, left + 60, bottom - 30);  //a
+            ctx.textAlign = "center";
+            ctx.fillText(b + units, right - 115, bottom + 20);   //b
+            ctx.textAlign = "left";
+            ctx.fillText(c + units, right, top + 150);   //c
+            ctx.textAlign = "center";
+            ctx.fillText("Diameter", right - 60, top + 80);
+            ctx.fillText(d + units, right - 60, top + 105);   //d
+            ctx.textAlign = "right";
+            ctx.fillText(e + units, left + 50, top + 50);  //e
+            suma += "$$\\begin{aligned}SA\\ of\\ triangular\\ prism&=2\\times triangular\\ end+2\\times " +  
+                        "sloping\\ sides+base\\\\[5pt]";
+            suma += "&=2\\times \\frac{1}{2}\\times" + b + "\\times" + c + "+2\\times" + a + "\\times" + e + 
+                        "+" + a + "\\times" + b + "\\\\[5pt]";
+            suma += "&=" + thouSep(satri, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ tube&=\\pi \\times diameter\\times length\\\\[5pt]";
+            suma += "&=\\pi \\times" + d + "\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(satube, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ end\\ caps&=2\\times \\pi \\times radius^2\\\\[5pt]";
+            suma += "&=2\\times \\pi \\times" + (d / 2) + "^2\\\\[5pt]";
+            suma += "&=" + thouSep(saend, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ total&=" + thouSep(satri, "\\ ") + "+" + thouSep(satube, "\\ ") + "-" + thouSep(saend, "\\ ") + "=\\underline{\\mathbf{" + 
+                        thouSep(dp(Number(satri) + Number(satube) - Number(saend), 3, 1), "\\ ") + unitsmath + "^2\\ (1\\ dp)}}\\\\[20pt]";
+            suma += "V\\ of\\ triangular\\ prism&=area\\ of\\ trangular\\ end\\times length\\\\[5pt]";
+            suma += "&=\\frac{1}{2}\\times" + b + "\\times" + c + "\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(vtri, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ of\\ tube&=\\pi \\times radius^2\\times length\\\\[5pt]";
+            suma += "&=\\pi \\times" + (d / 2) + "^2\\times" + a + "\\\\[5pt]";
+            suma += "&=" + thouSep(vtube, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ total&=" + thouSep(vtri, "\\ ") + "-" + thouSep(vtube, "\\ ") + "=\\underline{\\mathbf{" + 
+                        thouSep(dp(Number(vtri) - Number(vtube), 3, 1), "\\ ") + unitsmath + "^3\\ (1\\ dp)}}\\end{aligned}$$";
+            break;
+        case 5:     //rivet
+            sumq = "Find the surface area and volume of the object shown, rounding your answers to 1 decimal place. " + 
+                    "The head of the rivet is hemispherical. The drawing is not to scale";
+            units = " mm";      //only mm makes sense for rivet dimensions
+            unitsmath = "\\ mm";
+            a = rndgen(8, 15, 0, 1, -1);   //a int 8 to 15 head diameter
+            b = a / 2 + rndgen(-1, 1, 0, 1, -1);   //Shaft diameter = head diameter / 2 + rnd int -1 to 1
+            c = Math.ceil(a * 0.6 + rndgen(2, 6, 0, 1, -1));    //Shaft length = head diameter *  0.6 + rnd int 2 to 6
+            sahd = dp(3 * Math.PI * Math.pow(a / 2, 2), 3, 2);  //SA head
+            satube = dp(Math.PI * b * c, 3, 2);     //Shaft SA (- ends)
+            vhd = dp(2 / 3 * Math.PI * Math.pow(a / 2, 3), 3, 2);   //V head
+            vtube = dp(Math.PI * Math.pow(b / 2, 2) * c, 3, 2);     //V shaft
+            var img = document.getElementById("rivet");
+            ctx.drawImage(img, left, top, 350, 200);
+            ctx.textAlign = "center";
+            ctx.fillText(a + units, left + 170, top);
+            ctx.fillText(b + units, left + 170, bottom + 15);
+            ctx.textAlign = "left";
+            ctx.fillText(c + units, right - 80, bottom - 50);
+            suma += "$$\\begin{aligned}SA\\ of\\ rivet\\ head&=hemisphere\\ plus\\ circle\\\\[5pt]";
+            suma += "&=3\\pi r^2\\\\[5pt]";
+            suma += "&=3\\times \\pi \\times" + (a/2) + "^2 \\\\[5pt]";
+            suma += "&=" + thouSep(sahd, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ of\\ shaft&=tube\\ (area\\ of\\ 2\\ ends\\ hidden\\ in\\ joint) \\\\[5pt]";
+            suma += "&=\\pi Dl\\\\[5pt]";
+            suma += "&=\\pi \\times" + b + "\\times" + c + "\\\\[5pt]";
+            suma += "&=" + thouSep(satube, "\\ ") + unitsmath + "^2\\\\[5pt]";
+            suma += "SA\\ total&=" + thouSep(sahd, "\\ ") + "+" + thouSep(satube, "\\ ") + "=\\underline{\\mathbf{" + 
+                    thouSep(dp(Number(sahd) + Number(satube), 3, 1), "\\ ") + unitsmath + "^2 \\ (1\\ dp)}}\\\\[20pt]"; 
+            suma += "V\\ of\\ rivet\\ head&=\\frac{2}{3}\\pi r^3 \\\\[5pt]";
+            suma += "&=\\frac{2}{3}\\pi \\times" + (a/2) + "^3 \\\\[5pt]";
+            suma += "&=" + thouSep(vhd, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ of\\ shaft&=area\\ of\\ circle\\times length\\\\[5pt]";
+            suma += "&=\\pi r^2 l \\\\[5pt]";
+            suma += "&=\\pi \\times" + (b/2) + "^2 \\times" + c + "\\\\[5pt]";
+            suma += "&=" + thouSep(vtube, "\\ ") + unitsmath + "^3\\\\[5pt]";
+            suma += "V\\ total&=" + thouSep(vhd, "\\ ") + "+" + thouSep(vtube, "\\ ") + "=\\underline{\\mathbf{" + 
+                    thouSep(dp(Number(vhd) +  Number(vtube), 3, 1), "\\ ") + unitsmath + "^3 \\ (1\\ dp)}}\\end{aligned}$$"; 
+            break;
+    }
+    suma += "";
+    
+    document.getElementById("q").innerHTML = sumq;
+    document.getElementById("btnSoln").style.visibility="visible";
+}
---
+++ utils_maths_module.js
@@ -0,0 +1,229 @@
+//Various functions used in sum functions, as described in each below.
+
+var views = 0;  //Used to count solution views
+
+function op(sign) {
+//Used in solve1 to convert random number (0 or 1) to sign string.
+  if(sign) {
+    return "-";
+  } else {
+    return "+";
+  }
+}
+
+function cfchk(num, ltr, not1, notplus) {
+  //Covers all options for coefficients and signs.
+  //With/without 1's, with/without +'s, with/without ltr.
+  //Used in simultaneous functions, quadratics and solve1
+  if(num > 0) {     //num +ve
+    if(num === 1 && not1 && notplus) {
+      return ltr;
+    } else if(num === 1 && not1) {
+      return "+" + ltr;
+    } else if(notplus) {
+      return num + ltr;
+    } else {
+      return "+" + num + ltr;
+    }
+  } else {      //num -ve
+    if(num === -1 && not1) {
+      return "-" + ltr;
+    }
+    return num + ltr;
+  }
+}
+
+function eqnformat(id) {
+  //Re-runs mathjax rendering on text with given id. Used in all sum functions.
+  //Also toggles visibility of the 'a' element each time soln btn clicked, increments the views
+  //count each time 'a' is made visible and re-sets views to zero each time a question button is clicked.
+  MathJax.Hub.Queue(["Typeset",MathJax.Hub, id]);
+  if(id === "q") {
+    //Initialisation for new Q; reset number of views in soln btn and ensure 'a' element is hidden.
+    views = 0;
+    document.getElementById("btnSoln").innerHTML = "<span class='font-weight-bold'>Show/Hide Solution</span><br />Views : " + views;
+    document.getElementById("a").style.visibility="hidden";
+  }
+  if(id === "a") {      //soln btn clicked
+    if(window.getComputedStyle(document.getElementById("a")).visibility === "visible") {
+        document.getElementById("a").style.visibility="hidden";
+    } else {
+        document.getElementById("a").style.visibility="visible";
+        views += 1;
+        document.getElementById("btnSoln").innerHTML = "<span class='font-weight-bold'>Show/Hide Solution</span><br />Views : " + views
+    }
+  }
+}
+
+function countDecimals(value) {
+    //Counts number of decimal places. Used in rndgen() and dp()
+    if (Math.floor(value) !== value)
+        return value.toString().split(".")[1].length || 0;
+    return 0;
+}
+
+function rndgen(lower, upper, dp, step, fix) {
+  //Produces random numbers between limits, with set number of decimal places and selectable steps. Also,
+  //decimal places can be fixed.
+  //upper = largest num
+  //lower = smallest num
+  //dp = number of decimal places
+  //step = steps between smallest digits ie if 2 dp and want in 0.02 steps, then 0.02
+  //fix = number of dp's fixed. -1 if no trailing zeros wanted
+  step = step * Math.pow(10, dp);
+  if(fix === -1) {
+    do {
+      var tmp =  (Math.floor(Math.random() * ((upper * Math.pow(10, dp) / step) - 
+          (lower * Math.pow(10, dp) / step) + 1) + (lower * Math.pow(10, dp) / step)) / 
+          Math.pow(10, dp) * step);
+    } while(countDecimals(tmp) > dp) //Solves occasional float point maths error
+    return tmp;
+  } else {
+    return (Math.floor(Math.random() * (upper * Math.pow(10, dp) / step - 
+          lower * Math.pow(10, dp) / step + 1) + lower * Math.pow(10, dp) / step) / 
+          Math.pow(10, dp) * step).toFixed(fix);
+  }
+}
+
+function dp(num, scale, fix) {
+    if(!("" + num).includes("e")) {
+      if(fix === -1) {
+        return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+      } else {
+        return (+(Math.round(num + "e+" + scale)  + "e-" + scale)).toFixed(fix);
+      }
+    } else {
+      var arr = ("" + num).split("e");
+      var sig = ""
+      if(+arr[1] + scale > 0) {
+        sig = "+";
+      }
+      if(fix === -1) {
+        return +(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+      } else {
+        return (+(Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale)).toFixed(fix);
+      }
+    }
+  }
+
+function thouSep(value, sep) {
+    //Adds chosen 1 000's separator
+    return value.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, sep);
+}
+
+function gcd2(a, b) {
+  // Greatest common divisor of 2 integers
+  if(!b) return b===0 ? a : NaN;
+  return gcd2(b, a%b);
+}
+
+function gcd(array) {
+  // Greatest common divisor of a list (as array) of integers
+  var n = 0;
+  for(var i=0; i<array.length; ++i) {
+    n = gcd2(array[i], n);
+}
+  return n;
+}
+
+function lcm2(a, b) {
+  // Least common multiple of 2 integers
+  return a*b / gcd2(a, b);
+}
+
+function lcm(array) {
+  // Least common multiple of a list (as array) of integers
+  var n = 1;
+  for(var i=0; i<array.length; ++i) {
+    n = lcm2(array[i], n);
+  }
+  return n;
+}
+
+function chkpwr(ltr, pwr) {
+  //Used in hcflcm to format letter and power for display
+  //Removes letter if power is 0 and shows just letter if power is 1
+  if (pwr === 0) {
+    return "";
+  } else if (pwr === 1) {
+    return ltr;
+  } else {
+    return ltr + "^" + pwr;
+  }
+}
+
+function indchk(ltr, r, n, d, type) {
+//Used in Indices to format each term's index, depending on values of
+//radical, numerator and denominator.
+//'type' selects where used: 1 = question, 2 = initial answer breakdown, 3 = final answer breakdown
+  if (type === 1) {
+    if (r === 1) {
+      if (d === 1) {
+        return ltr + "^{" + n + "}";
+      } else if (d > 1) {
+        return ltr + "^{\\frac{" + n + "}{" + d + "}}";
+      }
+    } else if (r === 2) {
+      if (d === 1) {
+        return "\\sqrt{" + ltr + "^{" + n + "}}";
+      } else if (d > 1) {
+        return "\\sqrt{" + ltr + "^{\\frac{" + n + "}{" + d + "}}}";
+      }
+    } else if (r > 2) {
+      if (d === 1) {
+        return "\\sqrt[" + r + "]{" + ltr + "^{" + n + "}}";
+      } else if (d > 1) {
+        return "\\sqrt[" + r + "]{" + ltr + "^{\\frac{" + n + "}{" + d + "}}}";
+      }
+    }
+  } else if (type === 2) {
+    if (d > 1) {
+      if (r > 1) {
+        return ltr + "^{\\frac{" + n + "}{" + d + "\\times" + r + "}}";
+      } else {
+        return ltr + "^{\\frac{" + n + "}{" + d + "}}";
+      }
+    } else {
+      if (r > 1) {
+        return ltr + "^{\\frac{" + n + "}{" + r + "}}";
+      } else {
+        return ltr + "^{" + n + "}";
+      }
+    }
+  } else {
+    if (d > 1) {
+      if (r > 1) {
+        d = d * r;
+      }
+    } else {
+      if (r > 1) {
+        d = r;
+      }
+    }
+    if (d > 1) {
+      return "\\frac{" + n + "}{" + d + "}";
+    } else {
+      return n;
+    }
+  }
+}
+
+function sciengnot(num, pwr) {
+  //Used in numform to convert num & pwr to sci and eng forms
+  var logten = Math.floor(Math.log10(Math.abs(num)));
+  var scinum = dp(num / Math.pow(10, logten), 5, -1);
+  var scipwr = pwr + logten;
+  var scimod = scipwr - 3 * Math.floor(scipwr / 3); //excel version of modulus calc different to js % operator for -ve nums
+  var engnum = dp(scinum * Math.pow(10, scimod), 5, -1);
+  var engpwr = scipwr - scimod;
+  return [scinum, scipwr, engnum, engpwr];
+}
+
+function pwrzero(num, pwr) {
+  //Used in numform to show just the num if pwr is 0
+  if (pwr === 0) {
+    return num
+  } else {
+    return num + "\\times 10^{" + pwr + "}"
+  }
+}
